e369162998bfaa77c3220f95b8b68af05e6d1eee
Roberto <roberto.cuyar44@gmail.com>
2021-01-24 22:42:16 -0600

    Added NavBar component and test to check for the return value returning a defined result.

--- more-factory-app/src/components/App.js
+++ more-factory-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import NavBar from "./NavBar";
+
 
 const App = ()=> {
     return (
         <div>
-            More-Factory
+           <NavBar />
         </div>
     )
 }
---
+++ more-factory-app/test/components/NavBar.test.js
@@ -0,0 +1,8 @@
+import {expect} from "@jest/globals";
+
+const NavBar = require('../../src/components/NavBar');
+
+test("NavBar.js is defined", ()=>{
+    const n = null;
+    expect(n).toBeDefined();
+})
