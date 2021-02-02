5a1e06d0643cf23d9a8d026c2bde252659249d97
Roberto <roberto.cuyar44@gmail.com>
2021-02-01 23:10:27 -0600

    Addition of enzyme improvements on Navbar rendering and name containment unit testing.

--- more-factory-app/src/test/components/NavBar.test.js
+++ more-factory-app/src/test/components/NavBar.test.js
@@ -3,6 +3,8 @@ import React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import {shallow, configure } from "enzyme";
 import NavBar from "../../components/NavBar";
+import Typography from '@material-ui/core/Typography';
+
 import App from "../../components/App";
 
 // const NavBar = require('../../components/NavBar');
@@ -13,8 +15,16 @@ test("NavBar.js is defined", ()=>{
 
 configure({ adapter: new Adapter()});
 
+
+
 describe ("Navbar", ()=>{
     it("renders correctly", ()=>{
         shallow(<NavBar/>)
     })
+
+    it("Contains app name", ()=>{
+        const wrapper = shallow(<NavBar/>);
+        expect(wrapper.containsAnyMatchingElements([<Typography>More-Factory</Typography>])).toEqual(true);
+    })
 })
+
