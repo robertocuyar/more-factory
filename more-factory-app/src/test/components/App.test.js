d6bdd691575239282ba59942d1c19e2148d78b6f
Roberto <roberto.cuyar44@gmail.com>
2021-02-01 22:35:41 -0600

    Added enzyme testing to improve testing functionality overall.

--- more-factory-app/src/test/components/App.test.js
+++ more-factory-app/src/test/components/App.test.js
@@ -3,6 +3,5 @@ import {expect} from "@jest/globals";
 const App = require('../../components/App');
 
 test("App.js is defined", ()=>{
-    const n = null;
-    expect(n).toBeDefined();
+    expect(App).toBeDefined();
 })
--- more-factory-app/src/test/components/NavBar.test.js
+++ more-factory-app/src/test/components/NavBar.test.js
@@ -1,8 +1,20 @@
 import {expect} from "@jest/globals";
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import {shallow, configure } from "enzyme";
+import NavBar from "../../components/NavBar";
+import App from "../../components/App";
 
-const NavBar = require('../../components/NavBar');
+// const NavBar = require('../../components/NavBar');
 
 test("NavBar.js is defined", ()=>{
-    const n = null;
-    expect(n).toBeDefined();
+    expect(NavBar).toBeDefined();
+})
+
+configure({ adapter: new Adapter()});
+
+describe ("Navbar", ()=>{
+    it("renders correctly", ()=>{
+        shallow(<NavBar/>)
+    })
 })
