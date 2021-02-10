60443b7369d79d18437ba020aa40b20ad1e97a83
Roberto <roberto.cuyar44@gmail.com>
2021-02-10 13:54:34 -0600

    Added more-factory icon to navigation bar.

--- more-factory-app/src/components/NavBar.js
+++ more-factory-app/src/components/NavBar.js
@@ -6,6 +6,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import factoryIcon from "../img/more_factory_icon.png"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -27,7 +28,7 @@ export default function ButtonAppBar() {
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
+                        <img src={factoryIcon} alt=""/>
                     </IconButton>
                     <Typography variant="h6" className={classes.title}>
                         More-Factory
