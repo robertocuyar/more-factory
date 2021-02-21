1f28eae303070136c1ae721d3fae6cd6c4767163
Roberto <roberto.cuyar44@gmail.com>
2021-02-21 12:45:09 -0600

    Work on tab options completed along with testing.

--- more-factory-app/src/components/TabOptions.js
+++ more-factory-app/src/components/TabOptions.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 
-const TabOptions = ()=> {
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+    },
+});
+
+export default function CenteredTabs() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <div>
-            TabOptions
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                centered
+            >
+                <Tab label="Mine" />
+                <Tab label="Machines"/>
+                <Tab label="Build" />
+                <Tab label="Orders" />
+                <Tab label="Upgrades" />
+            </Tabs>
         </div>
-    )
+    );
 }
-
-export default TabOptions;
--- more-factory-app/src/test/components/TabOptions.test.js
+++ more-factory-app/src/test/components/TabOptions.test.js
@@ -1,7 +1,34 @@
 import {expect} from "@jest/globals";
 import React from 'react';
+import Adapter from "enzyme-adapter-react-16";
+import {shallow, configure} from "enzyme";
 import TabOptions from "../../components/TabOptions"
+import Tab from "@material-ui/core/Tab";
 
 test("TabOptions.js is defined", ()=>{
     expect(TabOptions).toBeDefined();
+});
+
+configure( {adapter: new Adapter()});
+
+describe ("TabOptions", ()=>{
+    let wrapper;
+
+    beforeEach(()=>{
+        wrapper = shallow(<TabOptions/>)
+    });
+
+    it("renders correctly", ()=>{
+        wrapper;
+    });
+
+    it("Contains all tab options for user to select from", ()=>{
+        expect(wrapper.containsAllMatchingElements([
+            <Tab label="Mine" />,
+            <Tab label="Machines"/>,
+            <Tab label="Build" />,
+            <Tab label="Orders" />,
+            <Tab label="Upgrades" />
+        ])).toEqual(true);
+    })
 })
