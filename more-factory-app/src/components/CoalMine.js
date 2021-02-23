c95e1dd9033ce5221ebe9bc71a7a2dcda9573548
Roberto <roberto.cuyar44@gmail.com>
2021-02-22 21:26:58 -0600

    Coal rock image design and avatar addition to CoalMine component.

--- more-factory-app/src/components/CoalMine.js
+++ more-factory-app/src/components/CoalMine.js
@@ -2,11 +2,17 @@ import React from "react";
 import {makeStyles} from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box"
+import Box from "@material-ui/core/Box";
+import Avatar from "@material-ui/core/Avatar";
+import coalImg from "../img/coal2.png";
 
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+    },
+    large: {
+        width: theme.spacing(8),
+        height: theme.spacing(8),
     }
 }));
 
@@ -17,7 +23,7 @@ const CoalMine = ()=>{
         <div className={classes.root}>
             <Grid container alignItems={'center'}>
                 <Grid item container xs={12} justify={'center'}>
-                    <Typography>Image</Typography>
+                   <Avatar alt={"coal"} src={coalImg} className={classes.large}/>
                     <Typography>Coal</Typography>
                 </Grid>
                 <Grid item container xs={12} justify={'center'}>
