6d5329d13c0890cc807d9bb0c7066e6b1dc5c5aa
Roberto <roberto.cuyar44@gmail.com>
2021-02-23 19:13:27 -0600

    Added coal image and pickaxe image for CoalMine component layout work.

--- more-factory-app/src/components/CoalMine.js
+++ more-factory-app/src/components/CoalMine.js
@@ -4,7 +4,9 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
 import coalImg from "../img/coal2.png";
+import pickImg from "../img/pickaxe.png"
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -13,6 +15,7 @@ const useStyles = makeStyles((theme) => ({
     large: {
         width: theme.spacing(8),
         height: theme.spacing(8),
+        margin: theme.spacing(2)
     }
 }));
 
@@ -23,13 +26,16 @@ const CoalMine = ()=>{
         <div className={classes.root}>
             <Grid container alignItems={'center'}>
                 <Grid item container xs={12} justify={'center'}>
-                   <Avatar alt={"coal"} src={coalImg} className={classes.large}/>
-                    <Typography>Coal</Typography>
+                   <Avatar alt={"Coal"} src={coalImg} className={classes.large}/>
+                   <Box display={"flex"} alignContent={"center"}>
+                    <h1>Coal</h1>
+                   </Box>
                 </Grid>
                 <Grid item container xs={12} justify={'center'}>
-                    <Typography>
-                        Button
-                    </Typography>
+                    <Button variant={"outlined"}>
+                        <span className={"button-text"}>Mine</span>
+                        <img className={"button-image"} src={pickImg} alt="Pickaxe"/>
+                    </Button>
                 </Grid>
                 <Grid item container xs={12} justify={'center'}>
                     <Typography>
--- more-factory-app/src/components/Field.js
+++ more-factory-app/src/components/Field.js
@@ -4,7 +4,6 @@ import Box from "@material-ui/core/Box"
 const Field = ()=>{
     return (
         <Box p={12} id={"field-img"}>
-            Field
         </Box>
     )
 }
