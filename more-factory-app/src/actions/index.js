552fa0e9bccdacda12544ae45f89ea43f05125f1
Roberto <roberto.cuyar44@gmail.com>
2021-02-24 20:35:03 -0600

    Added on the basic actions for mining Limestone and copper along with testing.

--- more-factory-app/src/actions/index.js
+++ more-factory-app/src/actions/index.js
@@ -1,4 +1,4 @@
-import {INVENTORY_SLOTS} from "./types";
+import {INVENTORY_SLOTS, MINE_COAL, MINE_COPPER, MINE_IRON, MINE_LIMESTONE} from "./types";
 
 export const inventorySlots = slots => {
     return {
@@ -8,13 +8,29 @@ export const inventorySlots = slots => {
 }
 
 export const mineCoal = coal => {
-
+    return {
+        type: MINE_COAL,
+        payload: coal
+    }
 }
 
 export const mineIron = iron => {
-
+    return {
+        type: MINE_IRON,
+        payload: iron
+    }
 }
 
 export const mineCopper = copper =>{
+    return {
+        type: MINE_COPPER,
+        payload: copper
+    }
+}
 
+export const mineLimestone = limestone =>{
+    return {
+        type: MINE_LIMESTONE,
+        payload: limestone
+    }
 }
--- more-factory-app/src/test/actions/index.test.js
+++ more-factory-app/src/test/actions/index.test.js
@@ -1,6 +1,7 @@
 import {expect} from "@jest/globals";
 import {inventorySlots, mineCoal, mineIron, mineCopper, mineLimestone} from "../../actions";
-import {INVENTORY_SLOTS} from "../../actions/types";
+import {INVENTORY_SLOTS, MINE_COAL, MINE_IRON, MINE_COPPER, MINE_LIMESTONE} from "../../actions/types";
+import {firstCoal, firstIron, firstCopper, firstLimestone} from "../../reducers/initialStates";
 
 test("inventorySlots to be defined", ()=>{
     expect(inventorySlots).toBeDefined();
@@ -17,10 +18,10 @@ test("mineCoal to be defined", ()=>{
    expect(mineCoal).toBeDefined();
 });
 
-test("inventorySlots to return payload with slot number", ()=>{
-    expect(mineCoal("coal")).toEqual({
-        type: INVENTORY_SLOTS,
-        payload: "coal"
+test("mineCoal to return correct payload", ()=>{
+    expect(mineCoal(firstCoal)).toEqual({
+        type: MINE_COAL,
+        payload: firstCoal
     });
 });
 
@@ -28,10 +29,31 @@ test("mineIron to be defined", ()=>{
     expect(mineIron).toBeDefined();
 });
 
+test("mineIron to return correct payload", ()=>{
+    expect(mineIron(firstIron)).toEqual({
+        type: MINE_IRON,
+        payload: firstIron
+    });
+});
+
 test("mineCopper to be defined", ()=>{
     expect(mineCopper).toBeDefined();
 });
 
+test("mineCopper to return correct payload", ()=>{
+    expect(mineCopper(firstCopper)).toEqual ({
+        type: MINE_COPPER,
+        payload: firstCopper
+    });
+})
+
 test ("mineLimestone to be defined", ()=>{
     expect(mineLimestone).toBeDefined();
 });
+
+test("mineLimestone to return correct payload", ()=>{
+    expect(mineLimestone(firstLimestone)).toEqual({
+        type: MINE_LIMESTONE,
+        payload: firstLimestone
+    });
+})
