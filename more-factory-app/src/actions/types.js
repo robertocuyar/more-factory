cf2156382d95a7a613131c5028b7d0c986e2b1f3
Roberto <roberto.cuyar44@gmail.com>
2021-02-24 19:23:46 -0600

    Added new types for each of the mining actions.

--- more-factory-app/src/actions/types.js
+++ more-factory-app/src/actions/types.js
@@ -1,2 +1,6 @@
 export const INVENTORY_SLOTS = "INVENTORY_SLOTS";
 export const SLOT_FILL = "SLOT_FILL";
+export const MINE_COAL = "MINE_COAL";
+export const MINE_IRON = "MINE_IRON";
+export const MINE_COPPER = "MINE_COPPER";
+export const MINE_LIMESTONE = "MINE_LIMESTONE";
--- more-factory-app/src/test/actions/types.test.js
+++ more-factory-app/src/test/actions/types.test.js
@@ -1,5 +1,5 @@
 import {expect} from "@jest/globals";
-import {INVENTORY_SLOTS, SLOT_FILL} from "../../actions/types";
+import {INVENTORY_SLOTS, SLOT_FILL, MINE_COAL, MINE_IRON, MINE_COPPER, MINE_LIMESTONE} from "../../actions/types";
 
 test("INVENTORY_SLOTS returns correct string", ()=>{
     expect(INVENTORY_SLOTS).toBe("INVENTORY_SLOTS");
@@ -7,4 +7,20 @@ test("INVENTORY_SLOTS returns correct string", ()=>{
 
 test("SLOT_FILL returns correct string", ()=>{
     expect(SLOT_FILL).toBe("SLOT_FILL");
+});
+
+test("MINE_COAL returns correct string", ()=>{
+    expect(MINE_COAL).toBe("MINE_COAL");
+});
+
+test("MINE_IRON returns correct string", ()=>{
+    expect(MINE_IRON).toBe("MINE_IRON");
+});
+
+test("MINE_COPPER returns correct string", ()=>{
+    expect(MINE_COPPER).toBe("MINE_COPPER");
+});
+
+test("MINE_LIMESTONE returns correct string", ()=>{
+    expect(MINE_LIMESTONE).toBe("MINE_LIMESTONE");
 });
