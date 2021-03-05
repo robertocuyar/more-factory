81edeffe020fc6571cc5911560e2deecd6cd8667
Roberto <roberto.cuyar44@gmail.com>
2021-03-05 15:08:54 -0600

    Completed functionality for inventory manager method once items are handed to the inventory array.

--- more-factory-app/src/test/methods/invManager.test.js
+++ more-factory-app/src/test/methods/invManager.test.js
@@ -10,17 +10,16 @@ const initialItem = {
     imgUrl: iron
 };
 
-
-
 test("invManager.js is defined", ()=>{
     expect(invManager).toBeDefined();
 });
 
 test("invManager returns a new inventory array, after taking in the default array (defaultBag), to include the initialItem as the first index.", ()=>{
-    expect(invManager(initialItem, defaultBag)[0].content).toEqual(initialItem.content);
-    console.log(invManager(initialItem, defaultBag));
-    expect(invManager(initialItem, defaultBag)[0].imgUrl).toEqual(initialItem.imgUrl);
-    expect(invManager(initialItem, defaultBag)[0].numContent).toEqual(20);
+    const inventory = invManager(initialItem, defaultBag);
+
+    expect(inventory[0].content).toEqual(initialItem.content);
+    expect(inventory[0].imgUrl).toEqual(initialItem.imgUrl);
+    expect(inventory[0].numContent).toEqual(20);
 });
 
 test("invManager keeps a maximum of 100 units of an item per slot and passes the remaining items to the next empty slot.", ()=>{
@@ -29,12 +28,14 @@ test("invManager keeps a maximum of 100 units of an item per slot and passes the
    firstItem.numContent = 90;
    firstItem.imgUrl = "";
 
-   expect(invManager(initialItem, defaultBag)[0].content).toEqual(initialItem.content);
-   expect(invManager(initialItem, defaultBag)[0].imgUrl).toEqual(initialItem.imgUrl);
-   expect(invManager(initialItem, defaultBag)[0].numContent).toEqual(100);
-   expect(invManager(initialItem, defaultBag)[1].content).toEqual(initialItem.content);
-   expect(invManager(initialItem, defaultBag)[1].imgUrl).toEqual(initialItem.imgUrl);
-   expect(invManager(initialItem, defaultBag)[1].numContent).toEqual(10);
+    const inventory = invManager(initialItem, defaultBag);
+
+    expect(inventory[0].content).toEqual(initialItem.content);
+   expect(inventory[0].imgUrl).toEqual(initialItem.imgUrl);
+   expect(inventory[0].numContent).toEqual(100);
+   expect(inventory[1].content).toEqual(initialItem.content);
+   expect(inventory[1].imgUrl).toEqual(initialItem.imgUrl);
+   expect(inventory[1].numContent).toEqual(10);
 });
 
 test("invManager does not replace an existing item with a different item type but places in a following null slot", ()=>{
@@ -43,8 +44,10 @@ test("invManager does not replace an existing item with a different item type bu
     firstItem.numContent = 30;
     firstItem.imgUrl = "";
 
-    expect(invManager(initialItem, defaultBag)[0].content).toEqual("Coal");
-    expect(invManager(initialItem, defaultBag)[1].content).toEqual("Iron");
+    const inventory = invManager(initialItem, defaultBag);
+
+    expect(inventory[0].content).toEqual("Coal");
+    expect(inventory[1].content).toEqual("Iron");
 });
 
 test("invManager updates item numContent in inventory when two different items are present.", ()=>{
@@ -57,11 +60,50 @@ test("invManager updates item numContent in inventory when two different items a
     secondItem.numContent = 95;
     secondItem.imgUrl = "";
 
-    expect(invManager(initialItem, defaultBag)[0].content).toEqual("Coal");
-    expect(invManager(initialItem, defaultBag)[1].content).toEqual("Iron");
-    expect(invManager(initialItem, defaultBag)[2].content).toEqual("Iron");
-    expect(invManager(initialItem, defaultBag)[1].numContent).toEqual(100);
-    expect(invManager(initialItem, defaultBag)[2].numContent).toEqual(15);
+    const inventory = invManager(initialItem, defaultBag);
+    expect(inventory[0].content).toEqual("Coal");
+    expect(inventory[1].content).toEqual("Iron");
+    expect(inventory[2].content).toEqual("Iron");
+    expect(inventory[1].numContent).toEqual(100);
+    expect(inventory[2].numContent).toEqual(15);
+
+});
+
+test("Given item will not change values if inventory slots are full or will provide an item with numContent remaining if given identical items are full", ()=>{
+
+    const [firstItem, secondItem, thirdItem, fourthItem, fifthItem, sixthItem, seventhItem, eightItem] = defaultBag.slots;
+    firstItem.content= "Limestone";
+    firstItem.numContent = 40;
+    secondItem.content ="Coal";
+    secondItem.numContent = 55;
+    thirdItem.content = "Limestone";
+    thirdItem.numContent = 23;
+    fourthItem.content = "Sulfur"
+    fourthItem.numContent = 77;
+    fifthItem.content = "Iron Plate";
+    fifthItem.numContent = 90;
+    sixthItem.content = "Copper Sheet";
+    sixthItem.numContent = 88;
+    seventhItem.content = "Concrete";
+    seventhItem.numContent = 33;
+    eightItem.content = "Iron";
+    eightItem.numContent = 95;
+
+    const testItem = {
+        content: "Copper",
+        numContent: 20,
+        imgUrl: ""
+    };
+
+    invManager(testItem, defaultBag);
+    expect(testItem.content).toEqual("Copper");
+    expect(testItem.numContent).toEqual(20);
+    expect(testItem.imgUrl).toEqual ("");
+
+    invManager(initialItem, defaultBag);
+    expect(initialItem.content).toEqual("Iron");
+    expect(initialItem.numContent).toEqual(15);
+    expect(initialItem.imgUrl).toEqual(iron);
 
 });
 
