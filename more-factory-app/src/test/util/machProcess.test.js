45a82105b8a9538ea0edaee4dff0392ba480fd7b
Roberto <roberto.cuyar44@gmail.com>
2021-03-20 19:49:38 -0500

    More completed testing and added functionality to machProcess to work with input and output checks for the specified machine.

--- more-factory-app/src/test/util/machProcess.test.js
+++ more-factory-app/src/test/util/machProcess.test.js
@@ -1,25 +1,82 @@
 import {expect} from "@jest/globals";
 import {machProcess} from "../../util/machProcess";
 import {defaultMachine} from "../../reducers/initialStates";
+import ironFurnace from "../../img/iron_furnace.png";
+import iron from "../../img/iron_inv.png";
+import coal from "../../img/coal_inv.png";
+import ironBar from "../../img/iron_bar.png";
 
 const machContent = 'Iron Furnace';
+const machContent2 = 'Bolt Assembler';
+const boltAssem =
+    {
+        content: machContent2,
+        description: "Uses 1 IRON INGOT to make 2 IRON BOLTS.",
+        imgUrl: "",
+        needsPower: false,
+        isOn: false,
+        process: 1500,
+        input: [
+            {
+                numContent: 0,
+                content: "Iron Ingot",
+                imgUrl: "",
+                use: 1
+            }
+        ],
+        output: [
+            {
+                numContent: 0,
+                content: "Iron Bolt",
+                imgUrl: "",
+                give: 2
+            }
+        ]
+    };
+
+ defaultMachine.machines[1] = boltAssem;
 const machineCur = defaultMachine;
+const machInput = defaultMachine.machines[0].input;
+const machOutput = defaultMachine.machines[0].output;
 
 test("machProcess.js is defined", ()=>{
    expect(machProcess).toBeDefined();
 });
 
 test("machProcess returns an object type.", ()=>{
-   expect(typeof machProcess()).toBe("object");
+   expect(typeof machProcess(machContent, machInput, machOutput, machineCur)).toBe("object");
 });
 
 test("machProcess with Iron Furnace takes away 1 coal and 1 iron input and returns 1 iron ingot output within the new machines object state",()=>{
     machineCur.machines[0].input[0].numContent = 5;
     machineCur.machines[0].input[1].numContent = 7;
-    const machUpdate = machProcess(machContent, machineCur).machines[0];
+    const machUpdate = machProcess(machContent, machInput, machOutput, machineCur).machines[0];
 
     expect(machUpdate.input[0].numContent).toBe(4);
     expect(machUpdate.input[1].numContent).toBe(6);
     expect(machUpdate.output[0].numContent).toBe(1);
+    expect(machUpdate.isOn).toBe(true);
 });
 
+test("machProcess with Iron Furnace won't process an output of Iron Ingot if there is an input of 1 Coal and 0 Iron Ore",()=>{
+    machineCur.machines[0].input[0].numContent = 1;
+    machineCur.machines[0].input[1].numContent = 0;
+    const machUpdate = machProcess(machContent,machInput, machOutput, machineCur).machines[0];
+
+    expect(machUpdate.input[0].numContent).toBe(1);
+    expect(machUpdate.input[1].numContent).toBe(0);
+    expect(machUpdate.output[0].numContent).toBe(0);
+    expect(machUpdate.isOn).toBe(false);
+});
+
+test("machProcess with Iron Furnace won't process input items if output slot is full with 80 iron ingot.",()=>{
+   machineCur.machines[0].input[0].numContent = 12;
+   machineCur.machines[0].input[1].numContent = 10;
+   machineCur.machines[0].output[0].numContent = 80;
+   const machUpdate = machProcess(machContent, machInput, machOutput, machineCur).machines[0];
+
+   expect(machUpdate.input[0].numContent).toBe(12);
+   expect(machUpdate.input[1].numContent).toBe(10);
+   expect(machUpdate.output[0].numContent).toBe(80);
+   expect(machUpdate.isOn).toBe(false);
+});
--- more-factory-app/src/util/machProcess.js
+++ more-factory-app/src/util/machProcess.js
@@ -1,17 +1,46 @@
-export const machProcess = (machContent, machineCur) => {
-    const newMachines = machineCur.machines.map(mach => {
-       if (mach.content === machContent){
-           mach.input = mach.input.map(input => {
-               input.numContent -= input.use;
-               return input;
-           });
-           mach.output = mach.output.map(output => {
-               output.numContent += output.give;
-               return output;
-           });
-       }
-           return mach;
-    });
+export const machProcess = (machContent, machInput, machOutput, machineCur) => {
 
-    return { machines: newMachines };
+    let continueIn = true;
+    let continueOut = true;
+    let newMachines = {};
+
+    for(let input of machInput){
+        if (input.numContent < input.use){
+            continueIn = false;
+            break;
+        }
+    }
+
+    for(let output of machOutput){
+        if (output.numContent === 80){
+            continueOut = false;
+            break;
+        }
+    }
+
+    if(continueIn && continueOut){
+         newMachines = machineCur.machines.map(mach => {
+            if (mach.content === machContent){
+                mach.isOn = true;
+                mach.input = mach.input.map(input => {
+                    input.numContent -= input.use;
+                    return input;
+                });
+                mach.output = mach.output.map(output => {
+                    output.numContent += output.give;
+                    return output;
+                });
+            }
+            return mach;
+        });
+
+    } else {
+        newMachines = machineCur.machines.map(mach => {
+            if (mach.content === machContent){
+                mach.isOn = false;
+            }
+            return mach;
+        });
+    }
+    return { machines: newMachines }
 }
